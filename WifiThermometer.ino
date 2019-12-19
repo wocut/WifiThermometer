a64d429068e40584e3b5865ce5a28d0207dddc9b
wocut9 <github@932168.com>
2019-12-19 23:50:33 +0800

    改正串口波特率

--- WifiThermometer.ino
+++ WifiThermometer.ino
@@ -6,10 +6,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <SoftwareSerial.h>
- int ID=0;
+ //int ID=0;
 int second=0,minute=0; 
-  bool send=0;
-  bool timeflag=0;
+  bool send=1;
+  bool timeflag=1;
   
 // 定义DS18B20数据口连接arduino的3号IO上
 #define ONE_WIRE_BUS 3
@@ -23,10 +23,12 @@ void setup(void)
 {
   // 设置串口通信波特率
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  //Serial.println("Dallas Temperature IC Control Library Demo");
   //Serial.println(mySerial.read());
- mySerial.begin(9600);
-
+  
+ mySerial.begin(9600);//因单片机无法支持115200
+                      //需要将esp8266的波特率改为9600
+                      //发送AT指令AT+CIOBAUD=9600 若成功则返回ok，否则返回error
  //控制ESP8266
  mySerial.println("AT+CWMODE=2"); 
  delay(500); 
@@ -40,7 +42,7 @@ void loop(void)
 
    sensors.requestTemperatures(); // 发送命令获取温度
    float RealTemp=sensors.getTempCByIndex(0);
-  String IDa ="AT+ CWSAP=\"";
+  String IDa ="AT+CWSAP=\"";
   int IDb;
 
   if (abs(RealTemp-MenberTemp)>1)    //如果温度比之前相差超过2度
@@ -54,20 +56,20 @@ void loop(void)
       {
         IDb=MenberTemp;
         //send=0;        //关闭wifi名称更新程序
-        send=1;        //临时试验新UNO
+        //send=1;        //临时试验新UNO
       }
 
-  String IDc ="c\",\"12K456u8\",11,3";
+  String IDc ="c\",\"12312312\",11,0";
   //String IDc ="c\"";
   String ID=IDa+IDb+IDc;
    //String ID="AT+CWSAP=\"baidc\",\"12K456u8\",11,3";   //临时试验新UNO
   second++;
-  if (second>=6)
+  if (second>=60)
     {
       second=0;
       minute++;
     }
-  if (minute>=1)    //定时2分钟
+  if (minute>2)    //定时2分钟
     {
       timeflag=1;
       minute=0;
@@ -77,23 +79,27 @@ void loop(void)
   {
       mySerial.println(ID);     //wifi名称更新程序
       
-      delay(1000);
-      mySerial.println("AT+RST");     //重启
-      delay(1000);
+      
+      //delay(1000);
+      //mySerial.println("AT+RST");     //重启
+      //delay(1000);
       send=0;              //关闭wifi名称更新程序
       timeflag=0; 
       //Serial.println(ID);
   }
 
-      Serial.print(minute );
-      Serial.print(second );
-
-      Serial.print(timeflag );
-      Serial.print(send );
-      Serial.print("RealTemp=");
-      Serial.print(RealTemp);
-      Serial.print("MenberTemp=");
-      Serial.println(MenberTemp);
+//      Serial.print("M:");
+//      Serial.print(minute );
+//      Serial.print("S:");
+//      Serial.print(second );
+//      Serial.print("time:");
+//      Serial.print(timeflag );
+//      Serial.print("temperture:");
+//      Serial.print(send );
+//      Serial.print("RealTemp=");
+//      Serial.print(RealTemp);
+//      Serial.print("MenberTemp=");
+//      Serial.println(MenberTemp);
   //mySerial.println(ID);
   //mySerial.println(IDb);
   //Serial.println(ID);
